cbb57cae02646b8b0e09868a7d72c7f77efa694a
Scott Hall <scotthall127@gmail.com>
2018-07-16 01:10:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(array)
+  line_number = array.count
+  line_list = []
+  x = 1
+  if line_number == 0
+    puts "The line is currently empty."
+  elsif line_number > 0
+    array.each do |person|
+      line_list << person.prepend("#{x}. ")
+      x += 1
+    end
+    puts "The line is currently: #{line_list}."
+  end
+end
